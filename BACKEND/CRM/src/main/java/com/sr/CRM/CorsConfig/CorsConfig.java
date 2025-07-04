b59a619b387c85995d02cbfdeaf3714ef9e1b2c0
ShivamRay3105 <shivamray3105@gmail.com>
2025-07-04 19:04:10 +0530

    backend modified

--- BACKEND/CRM/src/main/java/com/sr/CRM/CorsConfig/CorsConfig.java
+++ BACKEND/CRM/src/main/java/com/sr/CRM/CorsConfig/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Next.js frontend
+                .allowedOrigins("https://crm-lilac-omega.vercel.app") // Next.js frontend
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
